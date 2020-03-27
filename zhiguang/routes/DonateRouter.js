58970d291ff9259f582b2795f6a79021ac99fba8
unknown <drizzt79s@163.com>
2020-03-27 14:41:51 +0800

    2

--- zhiguang/routes/DonateRouter.js
+++ zhiguang/routes/DonateRouter.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('Donate');
+    var userData = req.session.userData;
+    if(!userData){
+        //用户未登录
+        res.render('Donate',{user:""});
+      }else{
+        //用户已登录
+        var user = userData.user_acct;
+        res.render('Donate', {user:user});
+      }
 });
 
 module.exports = router;
